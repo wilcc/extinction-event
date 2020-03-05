288f2ad10ae2ec56f87ec03c002f245c0e49acef
Wilson Chen <wilson.chen@codeimmersives.com>
2020-03-05 15:51:51 -0500

    finish

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const lis = document.querySelectorAll('ol')
+const lis = document.querySelectorAll('ol li')
 const lineThrough = function (event){
     event.target.style.textDecoration = 'line-through'
 }
 for (const li of lis){
     li.addEventListener('click',lineThrough)
 }
-const uls = document.querySelectorAll('ul')
+const uls = document.querySelectorAll('ul li')
 const transparent = function (event){
     event.target.style.opacity = '0'
 }
@@ -13,7 +13,7 @@ for (const li of uls){
     li.addEventListener('click',transparent)
 }
 
-const picture = document.querySelectorAll('section')
+const picture = document.querySelectorAll('section img')
 
 const display = function(events){
     events.target.style.width = '0px'
@@ -23,9 +23,21 @@ for (const img of picture){
     img.addEventListener('click', display);
 }
 
-const button = document.querySelector('#destroy-all')
+const button = document.querySelector('button')
 
+
+
+const destroyAll = function(){
+    for (const li of lis){
+    li.style.textDecoration = 'line-through'}
+    for (const ul of uls){
+        ul.style.opacity = '0'
+    }
+    for (const img of picture){
+        img.style.width = '0px'
+    }
+
+
+}
 button.addEventListener('click',destroyAll)
-
-
 
